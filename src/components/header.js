2cc73b775c6fd7f5fa9cc20cb751499d3dbfa89e
BrightTechnologies1 <silascyrax@gmail.com>
2022-07-18 23:26:26 +0100

    logo transition

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../App.css';
 import '../css/header.css';
 import logo from '../images/logo.png';
+import { motion } from "framer-motion"
 
 function Header() {
     return (
         <div className='header container'>
             <div className='row'>
                 <div className='logo'>
-                    <img src={logo} alt='' width={35}/>
+                    <motion.img initial={{ y: -100}} animate={{ y: 0}} transition={{ type: "spring", delay: 0.1, stiffness: 100 }} src={logo} alt='' width={35}/>
                 </div>
             </div>
         </div>
